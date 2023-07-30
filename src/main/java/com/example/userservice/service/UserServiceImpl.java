227afb7a689a84458656fbdfe13b1882a107e1e4
teddyrugged <jakpaetuk@gmail.com>
2023-07-30 21:14:17 +0100

    impl

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -2,10 +2,16 @@ package com.example.userservice.service;
 
 import com.example.userservice.model.Role;
 import com.example.userservice.model.User;
+import com.example.userservice.repository.RoleRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
+    private final UserService userRepo;
+    private final RoleRepo roleRepo;
     @Override
     public User saveUser(User user) {
         return null;
